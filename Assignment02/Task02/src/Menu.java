3d5272c76cb149e794e82c94d99e062192200077
nsovomaswanganyi <s224788213@mandela.ac.za>
2026-03-23 20:04:10 +0200

    I think i just finished task 2 of the pv assignment. The only thing left is implementing the mode logic

--- Assignment02/Task02/src/Menu.java
+++ Assignment02/Task02/src/Menu.java
@@ -1,30 +1,67 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /// Remember to push to origin
 
 public class Menu implements MenuChoice{
     /// Fields
     private String title;
-
-    public Menu() {
-    }
+    private ArrayList<MenuChoice> choices;
+    Scanner scanner = new Scanner(System.in);
 
     /// Class Methods
 
-
+    public Menu(String title) {
+        this.title = title;
+        choices = new ArrayList<>();
+    }
 
     public void addChoice(MenuChoice choice){
-
+        choices.add(choice);
     }
 
     protected void displayChoices(){
 
+        System.out.println(title);
+
+        for (int i = 0; i < choices.size(); i++) {
+            MenuChoice choice = choices.get(i);
+            String text = choice.getText();
+
+            if(choice instanceof Menu){
+                text += " (sub-menu)";
+            }
+            System.out.println((i+1) + ". " + text);
+        }
+        System.out.println((choices.size()+1)+ ". Exit");
     }
 
     protected int obtainChoice(){
-        return 0;
+        int choice = -1;
+
+        while(choice < 1 || choice > choices.size()+1){
+            System.out.println("Enter choice: ");
+
+            try{
+                choice = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number");
+            }
+        }
+        return choice;
     }
 
     protected void processChoice(int choice){
+        if(choice == choices.size()+1) {
+            //Exit Option
+            System.out.println("Exiting " + title);
+        } else {
+            MenuChoice selected = choices.get(choice - 1);
+            selected.run();
+        }
     }
+
+
     /// Interface Methods
     @Override
     public String getText() {
@@ -32,5 +69,13 @@ public class Menu implements MenuChoice{
     }
 
     @Override
-    public void run(){}
+    public void run(){
+        int choice;
+
+        do{
+            displayChoices();
+            choice = obtainChoice();
+            processChoice(choice);
+        } while( choice != choices.size() +1 );
+    }
 }
---
+++ Assignment02/Task02/src/Program.java
@@ -0,0 +1,155 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Program {
+    static void main() {
+        new Program();
+    }
+
+    public Program() {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+        Menu mainMenu = new Menu("Main Menu");
+
+        //First choice: Add Numbers
+        mainMenu.addChoice (
+                new MenuChoice() {
+                    @Override
+                    public String getText() {
+                        return "Add number: ";
+                    }
+
+                    @Override
+                    public void run() {
+                        Scanner scanner = new Scanner(System.in);
+                        System.out.println("Enter a number: ");
+
+                        try{
+                            int num = Integer.parseInt(scanner.nextLine());
+                            numbers.add(num);
+                        } catch (NumberFormatException e){
+                            System.out.println("Invalid number");
+                        }
+                    }
+                }
+        );
+
+        //Display
+        mainMenu.addChoice(
+                new MenuChoice() {
+                    @Override
+                    public String getText() {
+                        return "Display";
+                    }
+
+                    @Override
+                    public void run() {
+                        System.out.println("Numbers: " + numbers);
+                    }
+                }
+        );
+
+        //Clear Numbers
+        mainMenu.addChoice(
+                new MenuChoice() {
+                    @Override
+                    public String getText() {
+                        return "Clear numbers";
+                    }
+
+                    @Override
+                    public void run() {
+                        numbers.clear();
+                        System.out.println("List is now empty");
+                    }
+                }
+        );
+
+        //This is a sub-menu that handles the calc of mean, mode, and prime
+        Menu operationsMenu = new Menu("Operations");
+
+        //Get average
+        operationsMenu.addChoice(
+                new MenuChoice() {
+                    @Override
+                    public String getText() {
+                        return "Get the mean(average) of the numbers";
+                    }
+
+                    @Override
+                    public void run() {
+
+                        double count = 0, mean;
+                        double sum= 0;
+
+                        for (int i = 0; i < numbers.size(); i++) {
+                            count++;
+                            sum += numbers.get(i);
+                        }
+
+                        mean = sum/count;
+                        System.out.println("Mean: " + mean);
+
+                    }
+                }
+        );
+
+        //Get mode
+        operationsMenu.addChoice(
+                new MenuChoice() {
+                    @Override
+                    public String getText() {
+                        return "Get Mode: ";
+                    }
+
+                    @Override
+                    public void run() {
+
+                        
+                    }
+                }
+        );
+
+        //Get primes
+
+        operationsMenu.addChoice(
+                new MenuChoice() {
+                    @Override
+                    public String getText() {
+                        return "Get primes: ";
+                    }
+
+                    @Override
+                    public void run() {
+
+                        System.out.println("Primes: ");
+                        for (Integer number : numbers) {
+                            if (isPrime(number)) {
+                                System.out.print(number + " ");
+                            }
+                        }
+                        System.out.println();
+                    }
+
+                    private boolean isPrime(int n) {
+                        if(n<=1) return false;
+
+                        for (int i = 2; i <= Math.sqrt(n); i++) {
+                            if(n % 2 == 0) return false;
+                        }
+                        return true;
+                    }
+                }
+        );
+
+        mainMenu.addChoice(operationsMenu);
+
+        mainMenu.run();
+    }
+}
