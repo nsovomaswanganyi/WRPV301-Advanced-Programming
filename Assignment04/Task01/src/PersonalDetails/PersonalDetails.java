0ba107b23c05ffa6572d44ac9cec491b202d4ae7
nsovomaswanganyi <148325838+NsovoWMaswanganyi@users.noreply.github.com>
2026-05-05 15:30:45 +0200

    A lot going on with this JavaFX thing man

--- Assignment04/Task01/src/PersonalDetails/PersonalDetails.java
+++ Assignment04/Task01/src/PersonalDetails/PersonalDetails.java
@@ -8,6 +8,29 @@ import javafx.stage.Stage;
 
 /// DO NOT RUN THIS CLASS, USE THE RunPersonalDetails CLASS TO RUN!!!
 /// This ensures that you don't have to include VM options each and everytime
+/// I'm using the same strategy for all my tasks, so I might not paste this comment on all tasks
+///
+///
+///
+/// Read Up There
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
+/// Now that you've read above, Now you can mark!
+///
+///
+///
+///
+///
+///
+///
 public class PersonalDetails extends Application {
     @Override
     public void start(Stage stage) throws Exception {
